9f69cb3ac20932baa3297db82bd980c6d9cd0c7f
Biancaetec <161024767+Biancaetec@users.noreply.github.com>
2025-02-22 04:07:39 +0000

    payments atividade

--- src/controllers/payment.controller.js
+++ src/controllers/payment.controller.js
@@ -5,7 +5,30 @@ const PaymentController = {
     } catch (error) {
         res.status(500).send({ error: error.message });
     }
- }
+ },
+//  atividade pedida pelo Graziane:
+    async getPayments(req, res) {
+        try {
+            res.status(200).json({ message: 'Payments retrieved successfully' });
+        } catch (error) {
+            res.status(500).send({ error: error.message });
+        }
+    },
+
+    async updatePayment(req, res) {
+        try {
+            res.status(200).json({ message: 'Payment updated successfully' });
+        } catch (error) {
+            res.status(500).send({ error: error.message });
+        }
+    },
+    async deletePayment(req, res) {
+        try {
+            res.status(200).json({ message: 'Payment deleted successfully' });
+        } catch (error) {
+            res.status(500).send({ error: error.message });
+        }
+    }
 }
 
 export default PaymentController;
