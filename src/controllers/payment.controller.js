3f6501d2abed1085c6aea9db61b98b6e59d3e5a5
Biancaetec <161024767+Biancaetec@users.noreply.github.com>
2025-02-26 14:12:14 +0000

    feat comit concertar

--- src/controllers/payment.controller.js
+++ src/controllers/payment.controller.js
@@ -1,34 +1,37 @@
+import {z} from "zod"
+
+const PaymentSchema = z.object({
+    data: z.string().datetime(10, {message: "Pagamento inválido"}),
+    numerorecibo: z.number().int().positive(4, {message: "Número de recibo inválido"}),
+    usuarioId: z.number().int().positive(2, {message: "Id do usuário inválido"}),
+    valor: z.number().positive(), (4, {message: "Valor inválido"}),
+    observacao: z.string().max(100, {message: "Observação inválida"}),
+
+});
+
+
 const PaymentController = {
  async createPayment(req, res) {
     try {
+        const {data, numerorecibo, usuarioId, valor, observacao} = req.body;
+        PaymentSchema.parse({data, numerorecibo, usuarioId, valor, observacao});
+        console.log({data, numerorecibo, usuarioId, valor, observacao});
         res.status(201).json({ message: 'Payment created successfully' });
     } catch (error) {
+        if (error instanceof z.ZodError) {
+            return res.status(400).json({
+                message: "Erro de validação",
+                errors: error.errors.map(
+                    err => ({
+                        atributo: err.path[0],
+                        mensagem: err.message
+                    })
+                )
+            })
+        }
         res.status(500).send({ error: error.message });
     }
- },
-//  atividade pedida pelo Graziane:
-    async getPayments(req, res) {
-        try {
-            res.status(200).json({ message: 'Payments retrieved successfully' });
-        } catch (error) {
-            res.status(500).send({ error: error.message });
-        }
-    },
-
-    async updatePayment(req, res) {
-        try {
-            res.status(200).json({ message: 'Payment updated successfully' });
-        } catch (error) {
-            res.status(500).send({ error: error.message });
-        }
-    },
-    async deletePayment(req, res) {
-        try {
-            res.status(200).json({ message: 'Payment deleted successfully' });
-        } catch (error) {
-            res.status(500).send({ error: error.message });
-        }
-    }
+ }
 }
 
 export default PaymentController;
